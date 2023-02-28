a901970662555b417ffbfa7ab5dbced1ac2dc05a
varun dhand <varun@email.com>
2023-02-28 12:05:30 +0530

    trying out node

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
-const text = document.querySelector(".title"); //querySelector allows us to grab the element and store in a variable
-const changeColor = document.querySelector(".changeColor");
+const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 3000;
 
-// text.style.color = "red";
-// text.style.backgroundColor = "aqua";
-// text.classList.add('change'); // Adds classes
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
 
-function changeColorFunc(){
-  text.classList.toggle('change');
-}
-
-changeColor.addEventListener('click',changeColorFunc);
-
-const beatles = document.querySelectorAll('.beatles li');
-
-// console.log(beatles);
-
-for (beatle of beatles){
-  beatle.addEventListener('click',function(){
-    this.style.color = 'red';
-  })
-}
-
-const listInput = document.querySelector('.list-input');
-
-const addListBtn = document.querySelector('.addListbtn');
-
-addListBtn.addEventListener('click',function(){
-  
-})
-
-// console.log(listInput.value); // to see the value of input tag
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
--- notes.js
+++ notes.js
@@ -284,3 +284,38 @@ function doubleIt(item,index,arr){
 }
 
 console.log(numbers);
+//<------------------------DOM MANIPULATION---------------------------->
+
+const text = document.querySelector(".title"); //querySelector allows us to grab the element and store in a variable
+const changeColor = document.querySelector(".changeColor");
+
+
+// text.style.color = "red";
+// text.style.backgroundColor = "aqua";
+// text.classList.add('change'); // Adds classes
+
+function changeColorFunc(){
+  text.classList.toggle('change');
+}
+
+changeColor.addEventListener('click',changeColorFunc);
+
+const beatles = document.querySelectorAll('.beatles li');
+
+// console.log(beatles);
+
+for (beatle of beatles){
+  beatle.addEventListener('click',function(){
+    this.style.color = 'red';
+  })
+}
+
+const listInput = document.querySelector('.list-input');
+
+const addListBtn = document.querySelector('.addListbtn');
+
+addListBtn.addEventListener('click',function(){
+  
+})
+
+// console.log(listInput.value); // to see the value of input tag
